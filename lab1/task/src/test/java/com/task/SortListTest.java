a0b64de8e22f26b71a5eefa2cdc61aedddefa038
Baili <1172177657@qq.com>
2026-03-13 15:40:44 +0300

    update task 1 and 2

--- lab1/task/src/test/java/com/task/SortListTest.java
+++ lab1/task/src/test/java/com/task/SortListTest.java
@@ -3,6 +3,9 @@ package com.task;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+
 import static com.task.SortList.mergeSort;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,41 +15,99 @@ public class SortListTest {
     @DisplayName("проверка метод по алгоритму слияния")
     public void testSortList_1() {
         int[] input = {3, 2, 4, 1, 5};
-        int[] output = {1, 2, 3, 4, 5};
+        int[] expected = input.clone();
+        Arrays.sort(expected);
 
         mergeSort(input);
 
-        assertArrayEquals(output, input);
+        assertArrayEquals(expected, input);
     }
 
     @Test
     @DisplayName("проверка метод по алгоритму слияния, если есть одинаковые чисел")
     void testSortList_2() {
         int[] input = {4, 2, 8, 2, 5, 4, 1};
-        int[] output = {1, 2, 2, 4, 4, 5, 8};
+        int[] expected = input.clone();
+        Arrays.sort(expected);
 
         mergeSort(input);
-        assertArrayEquals(output, input);
+        assertArrayEquals(expected, input);
     }
 
     @Test
     @DisplayName("проверка метод по алгоритму слияния, если они уже в порядке")
     void testSortList_3() {
         int[] input = {1, 2, 3, 4, 5};
-        int[] output = {1, 2, 3, 4, 5};
+        int[] expected = input.clone();
+        Arrays.sort(expected);
 
         mergeSort(input);
-        assertArrayEquals(output, input);
+        assertArrayEquals(expected, input);
     }
 
     @Test
     @DisplayName("проверка метод по алгоритму слияния, если они в порядке убывания")
     void testSortList_4() {
         int[] input = {9, 7, 5, 3, 1};
-        int[] output = {1, 3, 5, 7, 9};
+        int[] expected = input.clone();
+        Arrays.sort(expected);
 
         mergeSort(input);
-        assertArrayEquals(output, input);
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    @DisplayName("проверка метод по алгоритму слияния, если только 1 элемент")
+    void testSortList_5() {
+        int[] input = {23};
+        int[] expected = input.clone();
+        Arrays.sort(expected);
+
+        mergeSort(input);
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    @DisplayName("проверка метод по алгоритму слияния, если только 2 элемент")
+    void testSortList_6() {
+        int[] input = {15, 21};
+        int[] expected = input.clone();
+        Arrays.sort(expected);
+
+        mergeSort(input);
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    @DisplayName("проверка метод по алгоритму слияния, если внутри есть мах или мин")
+    void testSortList_7() {
+        int[] input = {Integer.MAX_VALUE, Integer.MIN_VALUE, 0, -1, 1};
+        int[] expected = input.clone();
+        Arrays.sort(expected);
+
+        mergeSort(input);
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    @DisplayName("проверка метод по алгоритму слияния, если с большими количествами")
+    void testSortList_LargeData() {
+        int size = 10000;
+        int[] input = new int[size];
+        int[] expected = new int[size];
+
+        Random random = new Random(10);
+
+        for (int i = 0; i < size; i++) {
+            int value = random.nextInt();
+            input[i] = value;
+            expected[i] = value;
+        }
+
+        Arrays.sort(expected);
+        mergeSort(input);
+
+        assertArrayEquals(expected, input);
     }
 
     @Test
