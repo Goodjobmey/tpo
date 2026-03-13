d633c9f579aab61d491afeaa2d1ecd61980d6b84
Baili <1172177657@qq.com>
2026-03-13 12:14:18 +0300

    update task 2

--- lab1/task/src/main/java/com/task/SortList.java
+++ lab1/task/src/main/java/com/task/SortList.java
@@ -12,8 +12,12 @@ public class SortList {
     }
 
     private static void sortList(int[] arr, int l, int r) {
-        if (l >= r) return;
+        if (l >= r) {
+            return;
+        }
+
         int m = l + (r - l)/2;
+
         sortList(arr, l, m);
         sortList(arr, m+1, r);
 
@@ -23,26 +27,29 @@ public class SortList {
         int k = 0;
 
         while (i <= m && j <= r) {
+
             if (arr[i] <= arr[j]) {
                 temp[k] = arr[i];
                 i++;
-                k++;
             } else {
                 temp[k] = arr[j];
                 j++;
-                k++;
             }
+            k++;
         }
+
         while (i <= m) {
             temp[k] = arr[i];
             i++;
             k++;
         }
+
         while (j <= r) {
             temp[k] = arr[j];
             j++;
             k++;
         }
+
         System.arraycopy(temp, 0, arr, l, temp.length);
     }
 }
